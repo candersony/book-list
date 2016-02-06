e6ac931f8140604921245089dd24f927184a42b4
Craig Anderson <craig.anderson@talktalkplc.com>
2016-02-06 19:54:15 +0000

    Start the application from main.js

---
+++ app/main.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Craig on 05/02/2016.
+ */
+
+import BookController from './bookController.js';
+import BookListView from './bookListView.js';
+import SearchView from './searchView.js';
+
+function startApplication() {
+  let bookListView = new BookListView({containerElement: document.getElementById('book-list-container')})
+  let bookController = new BookController({bookListView: bookListView});
+  let searchView = new SearchView({
+    inputElement: document.getElementById('book-search-input'),
+    bookController: bookController,
+    defaultValue: 'javascript'
+  });
+}
+
+window.onload = startApplication;
