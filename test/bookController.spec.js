4b10738fe28a8abd44a14e3b97fed8d4d015e5de
Craig Anderson <craig.anderson@talktalkplc.com>
2016-02-06 17:45:56 +0000

    Added specs for the bookController and bookLoader

---
+++ test/bookController.spec.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Craig on 06/02/2016.
+ */
+
+describe('the book controller', () => {
+
+    it('should fetch 20 books at a time', () => {
+
+        expect(true).toBe(false);
+
+    });
+
+    it('should render books to the book list view', () => {
+
+        expect(true).toBe(false);
+
+    });
+});
---
+++ test/bookLoader.spec.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Craig on 06/02/2016.
+ */
+
+describe('the book loader', () => {
+
+    it('should request books with the specified query text', () => {
+
+        expect(true).toBe(false);
+
+    });
+
+    it('should request books with the specified results limit', () => {
+
+        expect(true).toBe(false);
+
+    });
+
+    it('should order the results as specified', () => {
+
+        expect(true).toBe(false);
+
+    });
+
+    it('should return a list of books', () => {
+
+        expect(true).toBe(false);
+
+    });
+
+    it('should return books with a title', () => {
+
+        expect(true).toBe(false);
+
+    });
+
+    it('should return books with a description no longer than 200 characters', () => {
+
+        expect(true).toBe(false);
+
+    });
+
+    it('should return books with a cover image', () => {
+
+        expect(true).toBe(false);
+
+    });
+});
